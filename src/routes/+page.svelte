4c6852a9f930292d770f5842bd28bfdae8f75692
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2023-12-30 11:30:26 +0900

    Apply ChatGPT site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+  <h1>Your Name</h1>
+  <p>Web Developer</p>
+</header>
+
+ <section>
+  <h2>About Me</h2>
+  <p>こんにちは！私は〇〇と申します。Web開発者として〇〇年の経験があります。...</p>
+</section>
+
+<section>
+  <h2>Projects</h2>
+  <ul>
+    <li><a href="#">Project 1</a> - 説明や技術スタックなど</li>
+    <li><a href="#">Project 2</a> - 説明や技術スタックなど</li>
+    <!-- 追加のプロジェクトをここに追加 -->
+  </ul>
+ </section>
+
+ <section>
+  <h2>Contact</h2>
+  <p>お問い合わせは以下のメールアドレスまでお気軽にどうぞ：<a href="mailto:your.email@example.com">your.email@example.com</a></p>
+</section>
+
+ <footer>
+  &copy; 2023 Your Name. All Rights Reserved.
+</footer>
+
+<style>
+  body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+  }
+
+  header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1em 0;
+  }
+
+  section {
+    max-width: 800px;
+    margin: 2em auto;
+    padding: 2em;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  h1, h2 {
+    color: #333;
+  }
+
+  p {
+    line-height: 1.6;
+    color: #555;
+  }
+
+  a {
+    color: #007BFF;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 0.5em;
+  }
+
+  footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1em 0;
+  }
+</style>

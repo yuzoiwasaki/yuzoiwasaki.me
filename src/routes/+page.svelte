0d9476925fd96353da2b1b20c9f19f4a8e9ae434
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2023-12-31 11:30:36 +0900

    Fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -124,7 +124,7 @@
 
  <section>
   <h2>Contact</h2>
-  <p>お問い合わせは以下のメールアドレスまでお気軽にどうぞ：<a href="yuzoiwasaki0929@gmail.com">yuzoiwasaki0929@gmail.com</a></p>
+  <p>お問い合わせは以下のメールアドレスまでお気軽にどうぞ：<a href="mailto:yuzoiwasaki0929@gmail.com">yuzoiwasaki0929@gmail.com</a></p>
  </section>
 
  <footer>

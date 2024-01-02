882428010a042d7766cfdab482108ced528896d8
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2024-01-02 18:34:22 +0900

    Create Header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import profile from '$lib/images/profile.jpg';
   import { page } from '$app/stores';
+  import Header from './Header.svelte';
   import Footer from './Footer.svelte';
 
   export let data;
@@ -21,16 +22,7 @@
   <meta property="og:image" content={profile} />
 </svelte:head>
 
-<header>
-  <img src={profile} alt="プロフィール画像">
-  <h1>Yuzo Iwasaki</h1>
-  <p>Software Engineer / Site Reliability Engineer / Engineering Manager and Other</p>
-
-  <div class="profile-icons">
-    <a href="https://twitter.com/yuzoiwasaki" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-    <a href="https://github.com/yuzoiwasaki" target="_blank"><i class="fa-brands fa-github"></i></a>
-  </div>
-</header>
+<Header />
 
 <section>
   <h2>About Me</h2>
@@ -155,32 +147,6 @@
     margin: 0;
     padding: 0;
     background-color: #f4f4f4;
-  }
-
-  header {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 2em 0;
-  }
-
-  header img {
-    border-radius: 50%;
-    width: 150px;
-    height: 150px;
-    object-fit: cover;
-    margin-bottom: 1em;
-  }
-
-  .profile-icons {
-    display: flex;
-    justify-content: center;
-    gap: 1em;
-  }
-
-  .profile-icons i {
-    font-size: 24px;
-    color: #007BFF;
   }
 
   :global(section) {
---
+++ src/routes/Header.svelte
@@ -0,0 +1,42 @@
+<script>
+  import profile from '$lib/images/profile.jpg';
+</script>
+
+<header>
+  <img src={profile} alt="プロフィール画像">
+  <h1>Yuzo Iwasaki</h1>
+  <p>Software Engineer / Site Reliability Engineer / Engineering Manager and Other</p>
+
+  <div class="profile-icons">
+    <a href="https://twitter.com/yuzoiwasaki" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+    <a href="https://github.com/yuzoiwasaki" target="_blank"><i class="fa-brands fa-github"></i></a>
+  </div>
+</header>
+
+<style>
+  header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 2em 0;
+  }
+
+  header img {
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+    object-fit: cover;
+    margin-bottom: 1em;
+  }
+
+  .profile-icons {
+    display: flex;
+    justify-content: center;
+    gap: 1em;
+  }
+
+  .profile-icons i {
+    font-size: 24px;
+    color: #007BFF;
+  }
+</style>

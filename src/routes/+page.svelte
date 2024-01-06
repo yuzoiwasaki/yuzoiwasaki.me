d2b3a1066b70e396c9c2cd876f149816a46e0839
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2024-01-06 18:52:20 +0900

    Create Skill component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import Header from './Header.svelte';
   import AboutMe from './AboutMe.svelte';
   import Experience from './Experience.svelte';
+  import Skill from './Skill.svelte';
   import Talk from './Talk.svelte';
   import Contact from './Contact.svelte';
   import Footer from './Footer.svelte';
@@ -32,23 +33,7 @@
 
 <Experience />
 
-<section>
-  <h2>Skills</h2>
-  <div class="skills-container">
-    {#each skills as skill}
-      <div class="skill-card">
-        <h3>{skill.name}</h3>
-        <p>{skill.year}</p>
-        <div class="skill-rating">
-          {#each {length: skill.star} as _}
-            <i class="fas fa-star"></i>
-          {/each}
-        </div>
-        <a href="/skills/{skill.id}">Read More</a>
-      </div>
-    {/each}
-  </div>
-</section>
+<Skill skills={skills} />
 
 <section class="article-container">
   <h2>Articles</h2>
@@ -146,39 +131,6 @@
 
   :global(a:hover) {
     text-decoration: underline;
-  }
-
-  .skills-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1em;
-    justify-content: center;
-  }
-
-  .skill-card {
-    max-width: 300px;
-    padding: 1em;
-    background-color: #f9f9f9;
-    border-radius: 5px;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-    margin: 1em;
-  }
-
-  .skill-card h3 {
-    color: #333;
-  }
-
-  .skill-card p {
-    color: #555;
-  }
-
-  .skill-rating {
-    margin-top: 0.5em;
-  }
-
-  .skill-rating i {
-    font-size: 18px;
-    color: #f8c200;
   }
 
   .article-container {
---
+++ src/routes/Skill.svelte
@@ -0,0 +1,56 @@
+<script>
+  export let skills;
+</script>
+
+<section>
+  <h2>Skills</h2>
+  <div class="skills-container">
+    {#each skills as skill}
+      <div class="skill-card">
+        <h3>{skill.name}</h3>
+        <p>{skill.year}</p>
+        <div class="skill-rating">
+          {#each {length: skill.star} as _}
+            <i class="fas fa-star"></i>
+          {/each}
+        </div>
+        <a href="/skills/{skill.id}">Read More</a>
+      </div>
+    {/each}
+  </div>
+</section>
+
+<style>
+  .skills-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+    justify-content: center;
+  }
+
+  .skill-card {
+    max-width: 300px;
+    padding: 1em;
+    background-color: #f9f9f9;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    margin: 1em;
+  }
+
+  .skill-card h3 {
+    color: #333;
+  }
+
+  .skill-card p {
+    color: #555;
+  }
+
+  .skill-rating {
+    margin-top: 0.5em;
+  }
+
+  .skill-rating i {
+    font-size: 18px;
+    color: #f8c200;
+  }
+</style>

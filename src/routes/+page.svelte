6be00e3108b8adcbfe9da04f60ffd9aac277ec58
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2024-01-06 11:05:13 +0900

    Create Experience component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import profile from '$lib/images/profile.jpg';
   import { page } from '$app/stores';
   import Header from './Header.svelte';
+  import Experience from './Experience.svelte';
   import Contact from './Contact.svelte';
   import Footer from './Footer.svelte';
 
@@ -30,24 +31,7 @@
   <p>{description}</p>
 </section>
 
-<section class="experience-container">
-  <h2>Experience</h2>
-  <div class="experience-card">
-    <h3>株式会社CAMPFIRE</h3>
-    <p>期間: 2016年 - 現在</p>
-    <p>業務内容: 前職の経営悪化によりサーバーサイドエンジニアとしてジョイン、当初はCakePHPでできたアプリケーションの開発に関わっていました。Ruby/Railsでのリプレイスを経て、SREに転向、SREチームの立ち上げとリード、マネージャーを経験しました。その後開発チームのマネージャーを経てVPoE/プロダクト開発部長に就任。マネジメントや採用、IT統制など幅広く関わっています。</p>
-  </div>
-  <div class="experience-card">
-    <h3>株式会社ワンズ</h3>
-    <p>期間: 2015年 - 2016年</p>
-    <p>業務内容: 新しいチャレンジを求めてWeb業界に転職し、プログラマとしておもにRailsアプリケーションの開発に従事しました。小さい会社だったためSEO担当も兼任しており、この時期にWeb開発の基礎とSEOについての深い知見を身に付けました。</p>
-  </div>
-  <div class="experience-card">
-    <h3>日本サード・パーティ株式会社</h3>
-    <p>期間: 2010年 - 2014年</p>
-    <p>業務内容: おもに HP Factory Express というサービスにインフラエンジニアとして関わっていました。キッティング、ラッキングからソフトウェアのインストール及び設定、トラブルシューティング、シェルスクリプトやVBAを使った業務効率化など様々な業務を行いました。特にLinux周りに関わることが多く、最終的にはソフトウェアチームのリーダーとして現場を率いていました。また、会社が資格取得を奨励していたこともあり、LPICやCCNAなどのベンダ資格を4年間で10個以上取得しました。</p>
-  </div>
-</section>
+<Experience />
 
 <section>
   <h2>Skills</h2>
@@ -171,24 +155,6 @@
 
   :global(a:hover) {
     text-decoration: underline;
-  }
-
-  .experience-container {
-    margin-top: 2em;
-  }
-
-  .experience-card {
-    border-left: 4px solid #007BFF;
-    padding-left: 1em;
-    margin-bottom: 1em;
-  }
-
-  .experience-card h3 {
-    color: #333;
-  }
-
-  .experience-card p {
-    color: #555;
   }
 
   .skills-container {
---
+++ src/routes/Experience.svelte
@@ -0,0 +1,38 @@
+<section class="experience-container">
+  <h2>Experience</h2>
+  <div class="experience-card">
+    <h3>株式会社CAMPFIRE</h3>
+    <p>期間: 2016年 - 現在</p>
+    <p>業務内容: 前職の経営悪化によりサーバーサイドエンジニアとしてジョイン、当初はCakePHPでできたアプリケーションの開発に関わっていました。Ruby/Railsでのリプレイスを経て、SREに転向、SREチームの立ち上げとリード、マネージャーを経験しました。その後開発チームのマネージャーを経てVPoE/プロダクト開発部長に就任。マネジメントや採用、IT統制など幅広く関わっています。</p>
+  </div>
+  <div class="experience-card">
+    <h3>株式会社ワンズ</h3>
+    <p>期間: 2015年 - 2016年</p>
+    <p>業務内容: 新しいチャレンジを求めてWeb業界に転職し、プログラマとしておもにRailsアプリケーションの開発に従事しました。小さい会社だったためSEO担当も兼任しており、この時期にWeb開発の基礎とSEOについての深い知見を身に付けました。</p>
+  </div>
+  <div class="experience-card">
+    <h3>日本サード・パーティ株式会社</h3>
+    <p>期間: 2010年 - 2014年</p>
+    <p>業務内容: おもに HP Factory Express というサービスにインフラエンジニアとして関わっていました。キッティング、ラッキングからソフトウェアのインストール及び設定、トラブルシューティング、シェルスクリプトやVBAを使った業務効率化など様々な業務を行いました。特にLinux周りに関わることが多く、最終的にはソフトウェアチームのリーダーとして現場を率いていました。また、会社が資格取得を奨励していたこともあり、LPICやCCNAなどのベンダ資格を4年間で10個以上取得しました。</p>
+  </div>
+</section>
+
+<style>
+  .experience-container {
+    margin-top: 2em;
+  }
+
+  .experience-card {
+    border-left: 4px solid #007BFF;
+    padding-left: 1em;
+    margin-bottom: 1em;
+  }
+
+  .experience-card h3 {
+    color: #333;
+  }
+
+  .experience-card p {
+    color: #555;
+  }
+</style>
